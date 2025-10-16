9c6c9ec7f50f21b898d9b3b444c01becf8a79e7a
Bengineer <blockengineer42@gmail.com>
2025-10-16 12:45:07 +0100

    refactor: streamline event fetching logic and improve variable usage

--- scripts/all-contract-events.js
+++ scripts/all-contract-events.js
@@ -3,32 +3,28 @@ import fs from "fs";
 const CONTRACT_ADDRESS =
   "0x02d26295d6c541d64740e1ae56abc079b82b22c35ab83985ef8bd15dc0f9edfb"; // Replace with your contract address
 const RPC_URL = "https://starknet-mainnet.public.blastapi.io";
-
+const from_block = 1460161;
 const provider = new Provider({ nodeUrl: RPC_URL });
-const events = [];
-let result = await provider.getEvents({
-  address: CONTRACT_ADDRESS,
-  chunk_size: 1024,
-  from_block: { block_number: 1460161 },
-});
-events.push(...result.events);
-let n = 0;
+
 const dirPath = "/home/ben/cgg/all-ba-events";
 const writeToFile = (data, index) => {
   fs.writeFileSync(`${dirPath}/${index}.json`, JSON.stringify(data, null, 2));
 };
+let n = 0;
+let continuation_token = "0";
 while (true) {
-  result = await provider.getEvents({
-    continuation_token: result.continuation_token,
+  const result = await provider.getEvents({
+    continuation_token,
     address: CONTRACT_ADDRESS,
     chunk_size: 1024,
-    from_block: { block_number: 1460161 },
+    from_block: { block_number: from_block },
   });
   if (result.events.length) {
     writeToFile(result, n);
   }
+  continuation_token = result.continuation_token;
   n++;
   console.log(
-    `n: ${n}, continuation_token: ${result.continuation_token} length: ${result.events.length}`
+    `n: ${n}, continuation_token: ${continuation_token} length: ${result.events.length}`
   );
 }
